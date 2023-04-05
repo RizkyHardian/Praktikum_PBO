6502d5eb5d7ed8a3ae5a9ea500d223502c7565fc
Rizky Hardian <92320728+RizkyHardian@users.noreply.github.com>
2023-04-05 15:13:11 +0800

    posttest 5 pbo

---
+++ posttest5/src/rpg/Npc.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rpg;
+
+/**
+ *
+ * @author Msi-GK
+ */
+public final class Npc extends Character {
+    private String job;
+    private int age;
+    private String dialog;
+
+    //constructor
+    public Npc(String isName, String isJob, String isGender, int isAge, String dialog) {
+        super(isName, isGender);
+        this.job = isJob;
+        this.age = isAge;
+        this.dialog = dialog;
+    }
+
+    //setter
+    public void setjob(String isJob) {
+        this.job = isJob;
+    }
+    public void setAge(int isAge) {
+        this.age = isAge;
+    }
+    public void setDialog(String dialog) {
+        this.dialog = dialog;
+    }
+    
+    //getter
+    public String getJob(){
+        return this.job;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getDialog() {
+        return dialog;
+    }
+
+    @Override
+    public final void display(){
+        System.out.println("  Name     : "+this.name);
+        System.out.println("  Gender   : "+this.gender);
+        System.out.println("  Job      : "+this.job);
+        System.out.println("  Age      : "+this.age);
+        System.out.println("\n \""+this.dialog+"\"");
+    }
+}
---
+++ posttest5/src/rpg/Player.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rpg;
+
+/**
+ *
+ * @author Msi-GK
+ */
+public final class Player extends Character {
+    private String role;
+    private int level;
+    private int hp;
+    private int str;
+    private final int max_level = 100;
+
+    //constructor
+    public Player(String isName, String isRole, String isGender, int isLevel, int isHp, int isStr) {
+        super(isName, isGender);
+        this.role = isRole;
+        if (isLevel > max_level) {
+            System.out.println("Level Player Anda Melebihi Max Level 100");
+            this.level = max_level;
+        } else {
+            this.level = isLevel;
+        }
+        this.hp = isHp;
+        this.str = isStr;
+    }
+    
+    //setter
+    public void setRole(String isRole){
+        this.role = isRole;
+    }
+    public void setLevel(int isLevel) {
+        if (isLevel > max_level) {
+            System.out.println("Level Player Anda Melebihi Max Level 100");
+            this.level = max_level;
+        } else {
+            this.level = isLevel;
+        }
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public void setStr(int str) {
+        this.str = str;
+    }
+
+    //getter
+    public String getRole() {
+        return role;
+    }
+    public int getLevel() {
+        return level;
+    }
+    public int getHealth() {
+        return hp;
+    }
+    public int getStrength() {
+        return str;
+    }
+    
+
+    @Override
+    public final void display(){    
+        System.out.println("  Name     : "+this.name);
+        System.out.println("  Gender   : "+this.gender);
+        System.out.println("  Role     : "+ this.role);
+    }
+
+    public final void display(boolean y){
+        if (y == true){
+            this.display();
+            System.out.println("\n  Stat Player");
+            System.out.println("  Level    :"+ this.level);
+            System.out.println("  Health   :"+ this.hp);
+            System.out.println("  Strength :"+ this.str);
+        } else {
+            this.display();
+        }
+    }
+}
